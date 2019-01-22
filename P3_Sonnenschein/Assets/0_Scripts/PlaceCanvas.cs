ffd9c3016762b43be1313eae8d68ca4c21775e5a
LucaHölzel <blankflankpictures@gmail.com>
2019-01-22 14:50:53 +0100

    FIIIIIX

---
+++ P3_Sonnenschein/Assets/0_Scripts/PlaceCanvas.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    private GameObject curMessage;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (curMessage == null)
+            {
+                curMessage = UI_Spawner.instance.spawn(UI_Types.ButtonIndicator, Ctrl_Buttons.X, "Place Canvas", GameObject.FindGameObjectWithTag("Player"), new Vector3(0, 2, 0));
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (curMessage != null)
+            {
+                GameObject.Destroy(curMessage);
+                curMessage = null;
+            }
+        }
+    }
+}
