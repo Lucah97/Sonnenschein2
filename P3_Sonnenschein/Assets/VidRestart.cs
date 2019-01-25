d28b281da0be6382b2c40248c1a3b1d19b72631a
Vince <vpowell@gmx.de>
2019-01-25 21:38:20 +0100

    Custom Doors my Nibbas

--- P3_Sonnenschein/Assets/VidRestart.cs
+++ P3_Sonnenschein/Assets/VidRestart.cs
@@ -6,23 +6,39 @@ public class VidRestart : MonoBehaviour {
 
     public bool isDead = false;
 
+    public bool customload = false;
+
+    public string CustomScene;
+
 	void Update () {
         if (GetComponent<Renderer>().enabled)
         {
+            if (customload)
+            {
+                LoadcustomScene(CustomScene);
+            }
+            else { 
             GameObject.FindGameObjectWithTag("Player").GetComponent<Collider>().enabled = false;
 
-            if (Input.GetButtonDown("Jump"))
-            {
-                if (isDead)
+                if (Input.GetButtonDown("Jump"))
                 {
-                    isDead = false;
-                    UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex);
-                }
-                else
-                {
-                    UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex + 1);
+                    if (isDead)
+                    {
+                        isDead = false;
+                        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex);
+                    }
+                    else
+                    {
+                        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex + 1);
+                    }
                 }
             }
         }
 	}
+
+    public void LoadcustomScene(string SceneName)
+    {
+        GameObject.FindGameObjectWithTag("Player").GetComponent<Collider>().enabled = false;
+        UnityEngine.SceneManagement.SceneManager.LoadScene(SceneName);
+    }
 }
