d8fa5f58e37b75a549409021e6357728f7164e32
LucaHölzel <blankflankpictures@gmail.com>
2019-02-05 00:22:32 +0100

    TV Intro Scene Test

---
+++ P3_Sonnenschein/Assets/TV_Camera.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TV_Camera : MonoBehaviour {
+
+    public RenderTexture renTex;
+
+	void Start () {
+        GetComponent<Camera>().targetTexture = renTex;
+	}
+	
+}
