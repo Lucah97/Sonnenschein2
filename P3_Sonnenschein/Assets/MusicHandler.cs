6e32cc57956c59aa4d575561f6a7ad59e7512a09
LucaHölzel <blankflankpictures@gmail.com>
2019-02-14 01:24:40 +0100

    Current State

---
+++ P3_Sonnenschein/Assets/MusicHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicHandler : MonoBehaviour {
+
+    public AudioClip musicMain;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<AudioSource>().PlayOneShot(musicMain);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!GetComponent<AudioSource>().isPlaying)
+        {
+            GetComponent<AudioSource>().PlayOneShot(musicMain);
+        }
+        if (Camera.main.transform.GetChild(0).GetComponent<Renderer>().enabled)
+        {
+            GetComponent<AudioSource>().Stop();
+            this.gameObject.SetActive(false);
+        }
+	}
+}
